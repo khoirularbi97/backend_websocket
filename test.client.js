d4d0214f5a41a91948559d2f80a31a2c88320343
khoirul arbi <145696604+khoirularbi97@users.noreply.github.com>
2026-06-24 00:09:42 +0700

    Update test.client.js

--- test.client.js
+++ test.client.js
@@ -2,31 +2,31 @@
 const fs = require('fs');
 const WebSocket = require('ws');
 
-const ws = new WebSocket('ws://localhost:5000');
+const ws = new WebSocket('wss://app.scurebot.site/ws/');
 
 ws.on('open', () => {
   console.log('Terhubung ke server');
 
-  const imageBuffer = fs.readFileSync('gambar.jpg');
-  const base64 = imageBuffer.toString('base64');
+  
 
   // Kirim gambar seperti dari ESP32
   ws.send(JSON.stringify({
-    type: 'image',
-    gate: 'masuk',  // atau 'keluar'
-    image: base64
+    type:'masuk',
+    uid: '7778888'
   }));
 });
 
 ws.on('message', (msg) => {
   try {
     const data = JSON.parse(msg);
-    if (data.type === 'hasil') {
-      console.log(`✅ Plat: ${data.plate}, Jenis: ${data.jenis}, Warna: ${data.warna}`);
-    } else {
-      console.log('Respon:', data);
-    }
-  } catch (e) {
-    console.log('Server:', msg.toString());
-  }
-});
+    const uid = data.uid;
+    const imageBuffer = fs.readFileSync('gambar.jpg');
+    const base64 = imageBuffer.toString('base64');
+    if (data.type === 'take_picture_in') {
+      console.log('ambil gambar');
+      ws.send(JSON.stringify({
+      type: 'image',
+      gate: 'masuk',  // atau 'masuk'
+      uid: uid,  // atau 'masuk'
+      image: base64
+  }));
